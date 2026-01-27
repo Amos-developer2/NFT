48f880cb8f32f6acdad07aef2a88bced6d9fb71d
ewald-ai <ewalda905@gmail.com>
2026-01-27 13:14:44 +0300

    Update collection.blade.php

--- resources/views/collection.blade.php
+++ resources/views/collection.blade.php
@@ -247,8 +247,8 @@
         -webkit-overflow-scrolling: touch;
         scrollbar-width: none;
         margin-bottom: 24px;
-        padding: 0;
-        gap: 10px
+        padding: 0!important;
+        gap: 10px!important;
     }
 
     .nft-grid::-webkit-scrollbar {
